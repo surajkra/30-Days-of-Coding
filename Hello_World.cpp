509747ded6cd08c61b385af9875aa858e7c7ec62
surajkra <surajkra@umich.edu>
2018-08-08 00:05:59 -0400

    Day 1 Update

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -3,8 +3,8 @@
 Objective
 In this challenge, we review some basic concepts that will get you started with
 this series. You will need to use the same (or similar) syntax to read input and
- write output in challenges throughout HackerRank. Check out the Tutorial tab
- for learning materials and an instructional video!
+write output in challenges throughout HackerRank. Check out the Tutorial tab
+for learning materials and an instructional video!
 
 Task
 To complete this challenge, you must save a line of input from stdin to a
