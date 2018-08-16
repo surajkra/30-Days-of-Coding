f433061e8173079c460d04e932ab4a75382b3dbb
surajkra <surajkra@umich.edu>
2018-08-15 23:55:43 -0400

    Day 1 Update

---
+++ Day - 1 Data Types.py
@@ -0,0 +1,28 @@
+
+'''
+Problem Statement:
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input
+Format section below) and initialize your  variables. Use the  operator to
+perform the following operations:
+Print the sum of 'i' plus your int variable on a new line.
+Print the sum of 'd' plus your double variable to a scale of one decimal place on a new line.
+Concatenate 's'  with the string you read as input and print the result on a new line.
+'''
+
+# Declare second integer, double, and String variables.
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Read and save an integer, double, and String to your variables.
+INTEGER = int(input())
+DOUBLE = float(input())
+STRING = str(input())
+# Print the sum of both integer variables on a new line.
+print(INTEGER+i)
+print(DOUBLE+d)
+print(s+STRING)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
